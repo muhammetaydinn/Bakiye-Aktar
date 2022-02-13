7d8432945cfdd143d60a2b3ce960b0962a6679e3
muhammetaydinn <muhammetaydin1704@gmail.com>
2022-02-13 18:52:19 +0300

    commit

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-
+import 'package:kirkuc_numara/constants/color_constant.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kirkuc_numara/models/card_model.dart';
 import '../../core/base/base_view.dart';
 import 'home_view_model.dart';
 
@@ -34,9 +36,71 @@ class HomeView extends StatelessWidget {
                   );
                 } else {
                   return Center(
-                    child: Text(
-                      'Kart var',
-                      style: Theme.of(context).textTheme.headline4,
+                    child: Container(
+                      height: 199,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.only(left: 16, right: 6),
+                        itemCount: cards.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            height: 199,
+                            width: 344,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(28),
+                                color: kBlueColor),
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  left: 29,
+                                  top: 48,
+                                  child: Text(
+                                    'KART NUMAARASI',
+                                    style: GoogleFonts.inter(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: kWhiteColor),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 29,
+                                  top: 65,
+                                  child: Text(
+                                    viewModel.cardData!.cardNumber.toString(),
+                                    style: GoogleFonts.inter(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w700,
+                                        color: kWhiteColor),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 29,
+                                  bottom: 45,
+                                  child: Text(
+                                    'KART SAHİBİ',
+                                    style: GoogleFonts.inter(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                        color: kWhiteColor),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 29,
+                                  bottom: 21,
+                                  child: Text(
+                                    cards[index].user,
+                                    style: GoogleFonts.inter(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w700,
+                                        color: kWhiteColor),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   );
                 }
